7b7fd143dfd22077f7f7e4db8755f5377deae130
Jeff Cho <jeff@Jeffs-MacBook-Pro.local>
2019-07-06 18:44:36 -0700

    [debug] changed div to p in the description component

--- client/src/Description.jsx
+++ client/src/Description.jsx
@@ -5,11 +5,11 @@ const Description = () => {
   return (
     <div className={Style.description_container}>
       <h2>How Pick-aroo works</h2>
-      <div>Pick-aroo alows you to input names to the selection pool, </div>
-      <div>then you can plick on the "next" button to pick a random name out</div>
-      <div>If you would like to delete the name from the pool</div>
-      <div>simply click on the name from the list, then the name would be removed.</div>
-      <div>happy selecting!!</div>
+      <p>Pick-aroo alows you to input names to the selection pool, </p>
+      <p>then you can plick on the "next" button to pick a random name out</p>
+      <p>If you would like to delete the name from the pool</p>
+      <p>simply click on the name from the list, then the name would be removed.</p>
+      <p>happy selecting!!</p>
     </div>
   )
 }
